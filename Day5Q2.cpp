b90e388c136549996c5910b489759d1e8d432803
eshwar9820 <72656633+eshwar9820@users.noreply.github.com>
2023-12-04 21:43:13 +0530

    Add files via upload

---
+++ Day5Q2.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int stockBuyAndSell(int n, vector<int> &prices) {
+        // code here
+        int stock = 0;
+        for(int i=0;i<n-1;i++){
+            if(prices[i+1]>prices[i]){
+                stock+=prices[i+1]-prices[i];
+            }
+        }
+        return stock;
+    }
+};
