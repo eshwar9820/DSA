f463936b5ac0e9d3f43bf637c7b00d1af2f97c25
eshwar9820 <72656633+eshwar9820@users.noreply.github.com>
2023-12-05 21:34:13 +0530

    Add files via upload

---
+++ Day6Q2.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int closest3Sum(int a[], int N, int X)
+    {
+        // code here
+        sort(a,a+N);
+        int msum=0,m=INT_MAX;
+        for(int i=0;i<N-2;i++){
+            int j=i+1,k=N-1;
+            while(j<k){
+                int sum = a[i]+a[j]+a[k];
+                if(sum==X){
+                    return sum;
+                }
+                else if(sum<X) j++;
+                else k--;
+                if(abs(X-sum)<m){
+                    m = abs(X-sum);
+                    msum=sum;
+                }
+            }
+        }
+        return msum;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int Arr[N];
+        for(int i=0;i<N;i++)    
+            cin>>Arr[i];
+        int X;
+        cin>>X;
+        Solution obj;
+        cout<<obj.closest3Sum(Arr, N, X)<<endl;
+    }
+    return 0;
+}
